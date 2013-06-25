7030d0e62ec6545bea1040f58805aa64cbad0f04
erelsgl <erelsgl@gmail.com>
2013-06-25 14:31:09 +0300

    convert 'getWords' to 'getFeatues', to allow more kinds of features

--- demo.js
+++ demo.js
@@ -3,7 +3,7 @@ var classifier = require('./');
 var bayes = new classifier.Bayesian();
 
 bayes.train("cheap replica watches", true);
-bayes.train("I don't know if this works on windows", false);
+bayes.train("I don't know if this works on windows replica", false);
 
 var category = bayes.classify("free watches"); 
 console.log(category);  // true
